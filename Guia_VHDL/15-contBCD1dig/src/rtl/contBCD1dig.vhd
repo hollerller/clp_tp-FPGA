90c1cd86989743c880f44631225219752402c432
hollerller <hollerller@gmail.com>
2024-08-10 13:16:20 -0500

    Behavioral 1 digit BCD cont

--- Guia_VHDL/15-contBCD1dig/src/rtl/contBCD1dig.vhd
+++ Guia_VHDL/15-contBCD1dig/src/rtl/contBCD1dig.vhd
@@ -2,7 +2,7 @@ library IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
 
-entity contBCD4b_2 is
+entity contBCD1dig is
     port (
         ena_i: in std_logic;
         rst_i: in std_logic;
@@ -10,9 +10,9 @@ entity contBCD4b_2 is
         q_o:   out std_logic_vector(3 downto 0)
     );
     
-end entity contBCD4b_2;
+end entity contBCD1dig;
 
-architecture contBCD4b_2_arch of contBCD4b_2 is
+architecture contBCD1dig_arch of contBCD1dig is
         -- declarations
 
       signal aux: std_logic_vector(3 downto 0) := "0000";   
@@ -26,9 +26,9 @@ begin
           if rst_i = '1' then
             aux <= "0000";
           elsif ena_i = '1' then
-            if aux = "1111" then
+            if aux = "1001" then
               aux <= "0000";
-            else
+            else  
             aux <= std_logic_vector(unsigned(aux) + 1);
             end if;
           end if; 
